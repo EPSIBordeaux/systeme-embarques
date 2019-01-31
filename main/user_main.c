e630e08861858965aa25cfe83a922b4cc8b441e5
Sylvain METAYER <contact@sylvainmetayer.fr>
2019-01-31 11:47:05 +0100

    feat: turn on and off light

--- main/user_main.c
+++ main/user_main.c
@@ -1,14 +1,17 @@
-/*
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
-
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+
+#include "driver/gpio.h"
+
+#include "esp_log.h"
 #include "esp_system.h"
+
+#define GPIO_OUTPUT_IO_0 12
 
 /******************************************************************************
  * FunctionName : app_main
@@ -19,4 +22,20 @@
 void app_main(void)
 {
     printf("SDK version:%s\n", esp_get_idf_version());
+
+    gpio_config_t c;
+    c.mode = GPIO_MODE_OUTPUT;
+    c.pin_bit_mask = GPIO_Pin_12;
+    c.intr_type = GPIO_INTR_DISABLE;
+    c.pull_down_en = c.pull_up_en = 0 ;
+    gpio_config(&c);
+
+    int cnt = 0;
+
+    while (1)
+    {
+        printf("cnt: %d\n", cnt++);
+        vTaskDelay(1000 / portTICK_RATE_MS);
+        gpio_set_level(GPIO_OUTPUT_IO_0, cnt % 2);
+    }
 }
